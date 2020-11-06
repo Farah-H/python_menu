5c33288c1ce001d88d46c6d3bc38dbf133061dea
farahmh <61236001+farahmh@users.noreply.github.com>
2020-11-06 16:19:47 +0000

    added take_order function to waitstaff class,

--- waitstaff_class.py
+++ waitstaff_class.py
@@ -8,9 +8,8 @@ from menu_class import Menu
 
 class Waitstaff(Menu):
 
-    def __init__(self, order): # takes in the user's order, which will be a list of items
+    def __init__(self): # takes in the user's order, which will be a list of items
         super().__init__()
-        self.order = order
 
     # function to display the user's order in a nice way
     def print_order(self):
@@ -18,12 +17,26 @@ class Waitstaff(Menu):
         for item in self.order:
             print(item) 
     
-    # function to ask the user
+    # function to ask the user what they would like to order
+    # would like to add more options to this if i have time, split it up by catagories
+    
+    def get_order(self):
+        self.order = []
+
+        while True:
+            order_item = input('What would you like to order? please enter "nothing" if you do not want anyhting(else)').lower()
+            if order_item == 'nothing':
+                break
+            else:
+                self.order.append(order_item)
+        return self.order
+
+
 
 
 # testing that the classes are linked fine, and that the function works using an example list
 
-# order = ['fruits', 'steak']
-# jenny = Waitstaff(order)
-# jenny.display_menu
+# jenny = Waitstaff()
+# jenny.display_menu()
+# print(jenny.get_order())
 # jenny.print_order()
