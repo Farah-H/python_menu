d225cef26a79be995fe13fc1bc81ea60c898f885
farahmh <61236001+farahmh@users.noreply.github.com>
2020-11-06 16:19:54 +0000

    Update run_menu.py

---
+++ run_menu.py
@@ -0,0 +1,12 @@
+from waitstaff_class import Waitstaff
+
+# This part of the program will actually execute taking an order, saving it, and printing it back to the user
+
+# we need to first display the menu to the user, our waitress Jenny will take do this
+your_orders = []
+if __name__ == "__main__":
+    pass
+else:
+    Jenny = Waitstaff()
+    Jenny.display_menu
+
